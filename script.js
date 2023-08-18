09b17cbd275275bd32201ea9a3f86436ff94bde8
nox1d <114980767+nox1d@users.noreply.github.com>
2023-08-18 16:24:07 +0800

    Add global variable for gridCell

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const container = document.getElementById('grid');
 
 let gridSize = 16*16
+let gridCell;
 
 for (i = 0; i < gridSize; i++) {
-    let gridCell = document.createElement('div');
+    gridCell = document.createElement('div');
     gridCell.classList.add('cell');
     container.appendChild(gridCell);
 }
