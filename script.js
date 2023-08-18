8025cfd0508bb3796d5700e2fdac54eb5f58aead
nox1d <114980767+nox1d@users.noreply.github.com>
2023-08-18 17:28:45 +0800

    Add button to resize canvas

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+const btn = document.getElementById('resize');
+let pixel;
+let gridSize = 0;
+btn.addEventListener('click', () => {
+    let resizePrompt = prompt('Enter the amount of pixel you want for each side (e.g. 16 -> 16x16), max = 100');
+
+    if (resizePrompt === null) {
+        return;
+    } else if (!parseInt(resizePrompt) || parseInt(resizePrompt) < 1) {
+        alert('Invalid, must be a positive number!');
+        return;
+    } else if (parseInt(resizePrompt) > 100) {
+        alert('The amount you have entered is above 100, too large');
+        return;
+    }
+    pixel = parseInt(resizePrompt);
+    gridSize = pixel*pixel;
+    return gridSize;
+});
+
 const container = document.getElementById('grid');
 
-let gridSize = 16*16
 let gridCell;
 
 for (i = 0; i < gridSize; i++) {
